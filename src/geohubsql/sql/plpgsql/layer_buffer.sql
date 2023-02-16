29a3c54e64d59c754dda9085a369ab26fa0f6531
rafd <ct@tommasitommasi.com>
2023-02-16 22:59:15 +0100

    better buffer behaivour in neighbouring tiles, input JSON arguments compliant with those used for the HDI function layer

--- src/geohubsql/sql/plpgsql/layer_buffer.sql
+++ src/geohubsql/sql/plpgsql/layer_buffer.sql
@@ -1,10 +1,29 @@
-CREATE OR REPLACE FUNCTION admin.layer_buffer(
+CREATE OR REPLACE FUNCTION admin.layer_buffer (
     z integer default 0,
     x integer default 0,
     y integer default 0,
     params varchar default '{
-              "input_layer_name":"provide_layer_name",
-              "buffer_distance":0
+                "input_layer_name":
+                { "id":"input_layer_name",
+                  "param_name":"input_layer_name",
+                  "type":"text",
+                  "icon":"fa-people-roof",
+                  "label":"Layer to be buffered in schema.table format",
+                  "widget_type":"search box",
+                  "value":"admin.input_layer",
+                  "hidden":0},
+                "buffer_distance":
+                { "id":"buffer_distance",
+                  "param_name":"buffer_distance",
+                  "type":"numeric",
+                  "icon":"fa-graduation-cap",
+                  "limits":{"min":0,"max":100000},
+                  "abs_limits":{"min":0,"max":100000},
+                  "value":0,
+                  "label":"Buffer radius/distance in meters",
+                  "widget_type":"slider",
+                  "hidden":0,
+                  "units":"meters"}
             }'
     )
 
@@ -21,6 +40,9 @@ RETURNS bytea AS $$
 		input_layer_name text;
 		buffer_distance float;
 		simplify_distance float;
+		--tile_buffer_distance float;
+		res_counter int;
+		sql_stmt text;
 		--sanitized_json jsonb;
 
 
@@ -28,7 +50,7 @@ RETURNS bytea AS $$
         --mvt:=admin.hdi_subnat_extarg(z,x,y,query_params::text);
         --mvt:=function_name(z,x,y,query_params::text);
 -- TODO
--- make the input params coherent with the structure used in function layers
+-- make the input params coherent with the structure used in function layers -- DONE
 -- what if buffer_distance <=0 ?
 -- what if geom column name is not 'geom' ?
 -- what about the attributes in the original layer? -> drop them, the buffer will be likely used as a binary mask
@@ -36,62 +58,43 @@ RETURNS bytea AS $$
 -- opt buffer distance taken from a field of the original layer?
 -- check geom type?
 
-        requested_json    := params::jsonb;
-        input_layer_name  := trim('"' FROM (requested_json->'input_layer_name')::text);
-        buffer_distance   := (requested_json->'buffer_distance')::float;
-        simplify_distance := buffer_distance/4;
+        requested_json       := params::jsonb;
+        input_layer_name     := trim('"' FROM (requested_json->'input_layer_name'->'value')::text);
+        buffer_distance      := (requested_json->'buffer_distance'->'value')::float;
+        simplify_distance    := buffer_distance/4;
+        --tile_buffer_distance := buffer_distance/2;
 
-        --RAISE WARNING 'OOOOOOOOOO input_layer_name: %, buffer_distance: %', input_layer_name, buffer_distance;
+
 
 		DROP TABLE IF EXISTS bounds;
         CREATE TEMPORARY TABLE bounds AS (
 			SELECT ST_TileEnvelope(z,x,y) AS geom
 		);
 
-
---SELECT
---       ST_AsMVTGeom(ST_Union(ST_Buffer(a.geom, 1000)), bounds.geom, extent => 2048, buffer => 256)) as geom
---FROM admin.water_facilities a;
-
-
---        DROP TABLE IF EXISTS temp_buffer;
---        CREATE TEMPORARY TABLE temp_buffer AS (
---            SELECT ST_Union(ST_Buffer(a.geom, 1000)) AS geom
---            FROM admin.water_facilities a
---        );
---
---        DROP TABLE IF EXISTS mvtgeom;
---        CREATE TEMPORARY TABLE mvtgeom AS (
---            SELECT ST_AsMVTGeom(t.geom, bounds.geom, extent => 2048, buffer => 256) AS geom
---            FROM (
---            SELECT ST_Union(ST_Buffer(a.geom, 1000)) AS geom
---            FROM admin.water_facilities a
---        ) t
---            JOIN bounds ON ST_Intersects(t.geom, bounds.geom)
---        );
---
---        SELECT ST_AsMVT(mvtgeom.*,'default', 2048, 'geom')
---		FROM AS mvtgeom
---		INTO mvt;
-
-
-
         DROP TABLE IF EXISTS temp_buffer;
 
-        EXECUTE format('
-        CREATE TEMPORARY TABLE temp_buffer AS (
-            SELECT ST_Buffer(ST_Simplify(a.geom,%s), %s) AS geom
-            FROM %s a
-            JOIN bounds AS b
-            ON ST_Intersects(a.geom, b.geom)
-        );',
-        simplify_distance,
-        buffer_distance,
-        input_layer_name
+        -- we need to buffer the tiles of the bounds table to include the buffers of features in neighbouring tiles.
+        sql_stmt = format('
+            CREATE TEMPORARY TABLE temp_buffer AS (
+                SELECT ST_Buffer(ST_Simplify(a.geom, %s), %s) AS geom
+                FROM %s a
+                JOIN bounds AS b
+                ON ST_Intersects(a.geom, ST_Buffer(b.geom, %s))
+                )',
+            simplify_distance,
+            buffer_distance,
+            input_layer_name,
+            buffer_distance
         );
 
--- no measurable effect
---         CREATE INDEX IF NOT EXISTS temp_geom_idx ON temp_buffer USING GIST (geom);
+        EXECUTE sql_stmt;
+
+--        RAISE WARNING '# # # # # # # # # # # # # # # input_layer_name: %',sql_stmt;
+--        SELECT COUNT(*) FROM temp_buffer INTO res_counter;
+--        RAISE WARNING '################# input_layer_name: %, buffer_distance: %, simplify_distance:%, res_counter:%', input_layer_name, buffer_distance, simplify_distance, res_counter;
+
+--        no measurable effect
+--        CREATE INDEX IF NOT EXISTS temp_geom_idx ON temp_buffer USING GIST (geom);
 
         DROP TABLE IF EXISTS temp_buffer_union;
         CREATE TEMPORARY TABLE temp_buffer_union AS (
@@ -107,17 +110,11 @@ RETURNS bytea AS $$
            JOIN bounds ON ST_Intersects(t.geom, bounds.geom)
        );
 
-       SELECT ST_AsMVT(mvtgeom.*,'default', 2048, 'geom')
-		FROM mvtgeom AS mvtgeom
+        SELECT ST_AsMVT(mvtgeom.*, 'admin.layer_buffer', 2048, 'geom')
+        FROM mvtgeom AS mvtgeom
 		INTO mvt;
 
---         SELECT ST_AsMVT(mvtgeom.*,'default', 2048, 'geom')
--- 		FROM (
---             SELECT ST_AsMVTGeom(t.geom, bounds.geom, extent => 2048, buffer => 256) AS geom
---             FROM temp_buffer t
---             JOIN bounds ON ST_Intersects(t.geom, bounds.geom)
---         ) AS mvtgeom
--- 		INTO mvt;
+
 
         RETURN mvt;
 
@@ -137,8 +134,16 @@ COMMENT ON FUNCTION admin.layer_buffer IS 'Buffer a vector layer by a given dist
 --            }') AS OUTP;
 
 --SELECT * FROM admin.layer_buffer(0,0,0,'{
+-- "input_layer_name":
+--   {"value":"admin.water_facilities"},
+-- "buffer_distance":
+--    {"value":1200}
+--           }') AS OUTP;
+
+--SELECT * FROM admin.layer_buffer(0,0,0,'{
 --  "input_layer_name":"admin.water_facilities",
 --  "buffer_distance":500}') AS OUTP;
 
 -- works in QGIS:
--- http://172.18.0.6:7800/admin.layer_buffer/{z}/{x}/{y}.pbf?params={"input_layer_name":"admin.water_facilities","buffer_distance":10007}
+-- http://172.18.0.6:7800/admin.layer_buffer/{z}/{x}/{y}.pbf?params={"input_layer_name":{"value":"admin.water_facilities"},"buffer_distance":{"value":1200}}
+-- http://172.18.0.6:7800/admin.layer_buffer/{z}/{x}/{y}.pbf
